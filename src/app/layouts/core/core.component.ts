0a4dad99c1130f0c01e5ac847bc17c0da102d80d
Jamil Pervaiz <jamilpervaiz.work@gmail.com>
2021-08-06 01:38:12 +0500

    refactor code

--- src/app/layouts/core/core.component.ts
+++ src/app/layouts/core/core.component.ts
@@ -1,33 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Entry } from 'src/app/models/entry';
 import { CoreService } from '../../services/core.service';
 
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
-  styleUrls: ['./core.component.css']
+  styleUrls: ['./core.component.css'],
 })
 export class CoreComponent implements OnInit {
   entries: Entry[] = [];
 
-  constructor(private coreService: CoreService) { }
+  constructor(@Inject(CoreService) private coreService: CoreService) {}
 
   ngOnInit(): void {
     this.init();
   }
 
   init() {
-    this.getData(); 
+    this.getData();
   }
 
   getData() {
-    this.coreService.get()
-    .subscribe((entries) => {
+    this.coreService.get().subscribe((entries) => {
       this.entries = this.sortEntries(entries);
     });
   }
 
   sortEntries(entries: Entry[]): Entry[] {
-    return entries.sort((x, y) => +new Date(y.published_at) - +new Date(x.published_at));
+    return entries.sort(
+      (x, y) => +new Date(y.published_at) - +new Date(x.published_at)
+    );
   }
 }
--- src/app/services/core.service.ts
+++ src/app/services/core.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { Entry } from '../models/entry';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoreService {
   private _url: string = `${environment.apiUrl}`;
-
-  constructor(private http: HttpClient) { }
+  constructor(@Inject(HttpClient) private http: HttpClient) {}
 
   get(): Observable<Entry[]> {
     return this.http.get<Entry[]>(this._url);
